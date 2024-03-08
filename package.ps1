2f6087b29fbfc1bf2939d19802c1fb5ed14cca3d
Shuv1Wolf <mskuratik06@gmail.com>
2024-03-08 18:56:40 +0300

    first

--- package.ps1
+++ package.ps1
@@ -21,9 +21,9 @@ try {
     docker-compose -f ./docker/docker-compose.yml up -d
 
     # Give the service time to start and then check that it's responding to requests
-    Start-Sleep -Seconds 10
-    Invoke-WebRequest -Uri http://localhost:8080/heartbeat
-    Invoke-WebRequest -Uri http://localhost:8080/v1/entities/get_entities -Method Post
+    Start-Sleep -Seconds 3
+    Invoke-WebRequest -Uri http://localhost:8082/heartbeat
+    
 
     Write-Host "The container was successfully built."
 } finally {
